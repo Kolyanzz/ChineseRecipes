51e4ec19b6a7a66b2a690b23a7b98bd0b919ac8b
Kolyanzz <guslyakov3343@gmail.com>
2021-04-23 11:25:12 +0900

    Нижняя навигация попытка 2 (неудачная)

--- app/src/main/java/com/kolumbo/chineserecipes/MainActivity.kt
+++ app/src/main/java/com/kolumbo/chineserecipes/MainActivity.kt
@@ -1,19 +1,76 @@
 package com.kolumbo.chineserecipes
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.kolumbo.chineserecipes.fragment.FragmentMain
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.kolumbo.chineserecipes.fragment.FavouritesFragment
+import com.kolumbo.chineserecipes.fragment.HomeFragment
+import com.kolumbo.chineserecipes.fragment.SettingsFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, FragmentMain.newInstance())
-                    .commitNow()
+
+        val homeFragment = HomeFragment()
+        val favouritesFragment = FavouritesFragment()
+        val settingsFragment = SettingsFragment()
+
+        setCurrentFragment(homeFragment)
+
+        Handler().postDelayed({
+            badgeSetup(R.id.nav_settings,7)
+        },2000)
+
+        badgeSetup(R.id.nav_favourites,20000)
+
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    setCurrentFragment(homeFragment)
+                    Log.i(TAG, "Home Selected")
+                    badgeClear(R.id.nav_home)
+                }
+                R.id.nav_favourites -> {
+                    setCurrentFragment(favouritesFragment)
+                    Log.i(TAG, "Favourites Selected")
+                    badgeClear(R.id.nav_favourites)
+                }
+                R.id.nav_settings -> {
+                    setCurrentFragment(settingsFragment)
+                    Log.i(TAG, "Settings Selected")
+                    badgeClear(R.id.nav_settings)
+                }
+            }
+            true
+        }
+
+    }
+
+    private fun badgeSetup(id: Int, alerts: Int) {
+        val badge = bottom_navigation.getOrCreateBadge(id)
+        badge.isVisible = true
+        badge.number = alerts
+    }
+
+    private fun badgeClear(id: Int) {
+        val badgeDrawable = bottom_navigation.getBadge(id)
+        if (badgeDrawable != null) {
+            badgeDrawable.isVisible = false
+            badgeDrawable.clearNumber()
         }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
---
+++ app/src/main/java/com/kolumbo/chineserecipes/fragment/SettingsFragment.kt
@@ -0,0 +1,21 @@
+package com.kolumbo.chineserecipes.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.kolumbo.chineserecipes.R
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class SettingsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_settings, container,false)
+
+
+}
